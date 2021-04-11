2eb1db5945bc28fae758d51369d465be699063bd
Slobodan Babic <slobodannbabic@gmail.com>
2021-04-11 17:19:45 +0200

    implemented TeacherServiceImpl class

--- src/main/java/com/alasdoo/developercourseassignment/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/alasdoo/developercourseassignment/service/impl/TeacherServiceImpl.java
@@ -1,37 +1,84 @@
 package com.alasdoo.developercourseassignment.service.impl;
 
 import com.alasdoo.developercourseassignment.dto.TeacherDTO;
+import com.alasdoo.developercourseassignment.entity.Teacher;
+import com.alasdoo.developercourseassignment.mapper.TeacherMapper;
+import com.alasdoo.developercourseassignment.repository.TeacherRepository;
+import com.alasdoo.developercourseassignment.service.TeacherService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-public class TeacherServiceImpl {
+public class TeacherServiceImpl implements TeacherService {
 
-    public TeacherDTO findOne(Integer id) {
-        return null;
-    }
+	@Autowired
+	private TeacherRepository teacherRepository;
 
-    public List<TeacherDTO> findAll() {
-        return null;
-    }
+	@Autowired
+	private TeacherMapper teacherMapper;
 
-    public TeacherDTO save(TeacherDTO teacherDTO) {
-        return null;
-    }
+	@Override
+	public TeacherDTO findOne(Integer id) {
+		Optional<Teacher> teacher = teacherRepository.findById(id);
+		if (!teacher.isPresent()) {
+			throw new IllegalArgumentException("Teacher with the following id = " + id + " is not found.");
+		}
+		return teacherMapper.transformToDTO(teacher.get());
+	}
 
-    public void remove(Integer id) throws IllegalArgumentException {
-    }
+	@Override
+	public List<TeacherDTO> findAll() {
+		return teacherRepository.findAll().stream().map(i -> teacherMapper.transformToDTO(i))
+				.collect(Collectors.toList());
+	}
 
-    public TeacherDTO update(Integer id, TeacherDTO teacherDTO) {
-        return null;
-    }
+	@Override
+	public TeacherDTO save(TeacherDTO teacherDTO) {
+		Teacher teacher = teacherMapper.transformToEntity(teacherDTO);
+		return teacherMapper.transformToDTO(teacherRepository.save(teacher));
+	}
 
-    public TeacherDTO findByTeacherNameAndTeacherSurname(String name, String surname) {
-        return null;
-    }
+	@Override
+	public void remove(Integer id) throws IllegalArgumentException {
+		Optional<Teacher> teacher = teacherRepository.findById(id);
+		if (!teacher.isPresent()) {
+			throw new IllegalArgumentException("Teacher with the following id = " + id + " is not found.");
+		}
+		teacherRepository.deleteById(id);
+	}
 
-    public TeacherDTO findByTeacherEmail(String email) {
-        return null;
-    }
+	public TeacherDTO update(Integer id, TeacherDTO teacherDTO) {
+		Optional<Teacher> oldTeacher = teacherRepository.findById(id);
+		if (!oldTeacher.isPresent()) {
+			throw new IllegalArgumentException("Teacher with the following id = " + id + " is not found.");
+		}
+		oldTeacher.get().setTeacherName(teacherDTO.getTeacherName());
+		oldTeacher.get().setTeacherSurname(teacherDTO.getTeacherSurname());
+		oldTeacher.get().setTeacherEmail(teacherDTO.getTeacherEmail());
+		teacherRepository.save(oldTeacher.get());
+		return teacherMapper.transformToDTO(oldTeacher.get());
+	}
+
+	@Override
+	public TeacherDTO findByTeacherNameAndTeacherSurname(String name, String surname) {
+		Optional<Teacher> teacher = teacherRepository.findByTeacherNameAndTeacherSurname(name, surname);
+		if (!teacher.isPresent()) {
+			throw new IllegalArgumentException("Teacher with the provided name and surname combination is not found.");
+		}
+		return teacherMapper.transformToDTO(teacher.get());
+	}
+
+	@Override
+	public TeacherDTO findByTeacherEmail(String email) {
+		Optional<Teacher> teacher = teacherRepository.findByTeacherEmail(email);
+		if (!teacher.isPresent()) {
+			throw new IllegalArgumentException("Teacher with the following email = " + email + " is not found.");
+		}
+		return teacherMapper.transformToDTO(teacher.get());
+	}
 }
