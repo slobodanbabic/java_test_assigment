a0e717296654260ea98e301ba3d8b64ae3b27488
Slobodan Babic <slobodannbabic@gmail.com>
2021-04-11 18:39:25 +0200

    remove unused import

--- src/main/java/com/alasdoo/developercourseassignment/controller/TeacherController.java
+++ src/main/java/com/alasdoo/developercourseassignment/controller/TeacherController.java
@@ -1,6 +1,5 @@
 package com.alasdoo.developercourseassignment.controller;
 
-import com.alasdoo.developercourseassignment.dto.StudentDTO;
 import com.alasdoo.developercourseassignment.dto.TeacherDTO;
 import com.alasdoo.developercourseassignment.service.impl.TeacherServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,42 +21,43 @@ import java.util.List;
 @CrossOrigin
 public class TeacherController {
 
-    @Autowired
-    TeacherServiceImpl teacherServiceImpl;
+	@Autowired
+	TeacherServiceImpl teacherServiceImpl;
 
-    @GetMapping(value = "/getTeacher/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public TeacherDTO selectTeacher(@PathVariable("id") Integer id) {
-        return teacherServiceImpl.findOne(id);
-    }
+	@GetMapping(value = "/getTeacher/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public TeacherDTO selectTeacher(@PathVariable("id") Integer id) {
+		return teacherServiceImpl.findOne(id);
+	}
 
-    @GetMapping(value = "/getAll", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<TeacherDTO> getAllTeachers() {
-        return teacherServiceImpl.findAll();
-    }
+	@GetMapping(value = "/getAll", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<TeacherDTO> getAllTeachers() {
+		return teacherServiceImpl.findAll();
+	}
 
-    @PostMapping(value = "/addTeacher", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public TeacherDTO saveTeacher(@RequestBody TeacherDTO teacherDTO) {
-        return teacherServiceImpl.save(teacherDTO);
-    }
-    
-    @DeleteMapping(value = "/delete/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void deleteTeacher(@PathVariable("id") Integer id) {
-    	teacherServiceImpl.remove(id);
-    }
-    
-    @PutMapping(value = "/update/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public TeacherDTO updateTeacher(@PathVariable("id") Integer id, @RequestBody TeacherDTO teacherDTO) {
-    	return teacherServiceImpl.update(id, teacherDTO);    	
-    }
+	@PostMapping(value = "/addTeacher", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public TeacherDTO saveTeacher(@RequestBody TeacherDTO teacherDTO) {
+		return teacherServiceImpl.save(teacherDTO);
+	}
 
-    @GetMapping(value = "/get/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public TeacherDTO findByEmail(@PathVariable("email") String email) {
-        return teacherServiceImpl.findByTeacherEmail(email);
-    }
+	@DeleteMapping(value = "/delete/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void deleteTeacher(@PathVariable("id") Integer id) {
+		teacherServiceImpl.remove(id);
+	}
 
-    @GetMapping(value = "/get/{name}/{surname}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public TeacherDTO findByNameAndSurname(@PathVariable("name") String name, @PathVariable("surname") String surname) {
-        return teacherServiceImpl.findByTeacherNameAndTeacherSurname(name, surname);
-    }
+	@PutMapping(value = "/update/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public TeacherDTO updateTeacher(@PathVariable("id") Integer id, @RequestBody TeacherDTO teacherDTO) {
+		return teacherServiceImpl.update(id, teacherDTO);
+
+	}
+
+	@GetMapping(value = "/get/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public TeacherDTO findByEmail(@PathVariable("email") String email) {
+		return teacherServiceImpl.findByTeacherEmail(email);
+	}
+
+	@GetMapping(value = "/get/{name}/{surname}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public TeacherDTO findByNameAndSurname(@PathVariable("name") String name, @PathVariable("surname") String surname) {
+		return teacherServiceImpl.findByTeacherNameAndTeacherSurname(name, surname);
+	}
 
 }
