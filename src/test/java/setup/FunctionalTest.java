8b897dfa70310b37d2d0d8c6996e695728147cf6
Slobodan Babic <slobodannbabic@gmail.com>
2021-04-12 23:01:30 +0200

    added dependencies for Selenium and JUnit5 and added FunctionalTest,PageObject classes

---
+++ src/test/java/setup/FunctionalTest.java
@@ -0,0 +1,27 @@
+package setup;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * <strong>FunctionalTest</strong> handles setup and teardown of WebDriver.
+ * 
+ * @author Kim Schiller
+ */
+public class FunctionalTest {
+
+	protected static WebDriver driver;
+
+	@BeforeAll
+	public static void setUp() {
+		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+
+	@AfterAll
+	public static void tearDown() {
+		driver.close();
+	}
+}
